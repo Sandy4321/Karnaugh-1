76d9f333bf52bf5295e83c86d1090e827352681e
green <green@AlienHominid.(none)>
2014-05-09 18:42:24 -0400

    Added karnaughMap function (will be extended later) and tests for it. Added a function to find the largest value in a 2d array.

--- karnaugh.py
+++ karnaugh.py
@@ -29,11 +29,47 @@ def karnaughList(bits):
 # Gets the bit, b, of a number, n
 # 0 indexed
 # big endian
-# Parameter(s)
+# Parameter(s):
 def getBit(n, b):
     mod = 2 ** b
     return (n % (2 * mod)) / mod 
 
+# Just a function with 2 arguments
+# Solely for testing purposes
+# Parameters(2):
+#     a is an integer
+#     b is an integer
+def twoArgumentFunction(a, b):
+    return a + b
+
+# Makes the Karnaugh map of twoArgumentFunction
+# that has 2 inputs whose maximum size, in bits,
+# is inputSize
+# Will be extended to other functions later
+# Will be extended to accept inputs of different
+# sizes later
+# Parameter(s):
+#     inputSize is an integer
+def karnaughMap(inputSize):
+    karList = karnaughList(inputSize)
+    karMap = []
+    for i in karList:
+        row = []
+        for j in karList:
+            val = twoArgumentFunction(i, j)
+            row.append(val)
+        karMap.append(row)
+    return karMap
+
+# Finds the largest value in the map
+# Parameter(s):
+#     array is a list of lists
+def mapMax(array):
+    m = array[0][0]
+    for i in array:
+        if(max(i) > m):
+            m = max(i)
+    return m
 
 def main():
     if(len(sys.argv) == 2):
@@ -44,7 +80,7 @@ def main():
             testing.equality(l, rev(rl))
             testing.inequality(rev(l), rev(rl))
 
-            # karnaugh tests
+            # karnaughList tests
             kl1 = [0, 1]
             kl2 = [0, 1, 3, 2]
             kl3 = [0, 1, 3, 2, 6, 7, 5, 4]
@@ -63,13 +99,27 @@ def main():
                 n = (2 ** 16) - 1
                 testing.equality(getBit(n, i), 1)
 
+            # twoArgumentFunction tests
+            testing.equality(twoArgumentFunction(1, 2), 3)
+            testing.equality(twoArgumentFunction(3, 5), 8)
+            testing.equality(twoArgumentFunction(6, 6), 12)
+
+            # karnaughMap tests
+            km1 = [[0, 1], [1, 2]]
+            km2 = [[0, 1, 3, 2], [1, 2, 4, 3], [3, 4, 6, 5], [2, 3, 5, 4]]
+            testing.equality(karnaughMap(1), km1)
+            testing.equality(karnaughMap(2), km2)
+
+            # mapMax tests
+            testing.equality(mapMax(karnaughMap(1)), 2)
+            testing.equality(mapMax(karnaughMap(2)), 6)
+            testing.equality(mapMax(karnaughMap(3)), 14)
+            testing.equality(mapMax(karnaughMap(4)), 30)
+
             testing.main()
         else:
-            print karnaugh(int(sys.argv[1]))
-    else:
-        print "Use: $ python karnaugh.py i\n"
-        print "Where i is the minimum number of bits needed to"
-        print "express the largest number in the list."
+            for i in karnaughMap(int(sys.argv[1])):
+                print i
 
 if __name__ == "__main__":
     main()
