2568d83d0bfcb1562676a0e87066c01b309eac59
pelynch <pelynch@wpi.edu>
2014-05-09 12:35:34 -0400

    So I guess that there is a home-made testing package now

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ zoccihedron
 I just like Karnaugh maps. I've used them to help me create redstone circuitry in Minecraft before. I have never thrown much code at it which has resulted in me making 16 x 16 tables. 
 
 There isn't really a direction this is going yet but I have a rough outline of what I want this to do.
+
+I guess there's also a testing module now.
---
+++ testing.py
@@ -0,0 +1,50 @@
+# testing.py
+# 2014-05-09+15:17:00
+
+# I do not currently know a better way to do this
+global passed
+global failed
+passed = 0
+failed = 0
+
+# Tests if item1 and item2 are equal
+# Parameter(s):
+#     item1 is an object
+#     item2 is an object
+def equality(item1, item2):
+    global passed
+    global failed
+    if(item1 == item2):
+        passed = passed + 1
+        return True
+    else:
+        failed = failed + 1
+        print "\nTest " + str(passed + failed) + ":"
+        print "\t" + str(item1) + " does not equal " + str(item2)
+        print "even though it should.\n"
+        return False
+
+# Tests if item1 and item2 are unequal
+# Parameter(s):
+#     item1 is an object
+#     item2 is an object
+def inequality(item1, item2):
+    global passed
+    global failed
+    if(item1 != item2):
+        passed = passed + 1
+        return True
+    else:
+        failed = failed + 1
+        print "\nTest " + str(passed + failed) + ":"
+        print "\t" + str(item1) + " equals " + str(item2)
+        print "even though it should not.\n"
+        return False
+
+def main():
+    print str(passed + failed) + " tests ran."
+    print str(passed) + " tests passed."
+    print str(failed) + " tests failed."
+
+if __name__ == "__main__":
+    main()
